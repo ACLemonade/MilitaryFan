963d744ab558f9099f9998f6cc8ca590b842a6ed
lemonade <lemonade@email.com>
2016-08-30 22:17:42 +0800

    基本登录功能实现

--- MilitaryFan/LoginViewController.m
+++ MilitaryFan/LoginViewController.m
@@ -19,6 +19,10 @@
 
 @implementation LoginViewController
 #pragma mark - 协议方法 UITextField Delegate
+- (void)textFieldDidEndEditing:(UITextField *)textField{
+    
+    
+}
 - (BOOL)textFieldShouldReturn:(UITextField *)textField{
     if (textField == _accountTF) {
         [_accountTF endEditing:YES];
@@ -30,10 +34,42 @@
     return YES;
 }
 #pragma mark - 方法 Methods
+- (IBAction)userLogin:(id)sender {
+    BmobQuery *loginQuery = [BmobQuery queryWithClassName:@"UserInfo"];
+    loginQuery.limit = 1;
+    [loginQuery whereKey:@"userName" equalTo:_accountTF.text];
+    [loginQuery whereKey:@"password" equalTo:_passwordTF.text];
+    [loginQuery findObjectsInBackgroundWithBlock:^(NSArray *array, NSError *error) {
+        if (error) {
+            NSLog(@"错误信息: %@",error);
+        }else{
+            if (array.firstObject) {
+                //登录成功,弹出提示框
+                UIAlertController *alertVC = [UIAlertController alertControllerWithTitle:@"" message:@"登录成功" preferredStyle:UIAlertControllerStyleAlert];
+                UIAlertAction *yesAction = [UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+                    //点击确定返回导航控制器根视图
+                    [self.navigationController popToRootViewControllerAnimated:YES];
+                }];
+                [alertVC addAction:yesAction];
+                [self presentViewController:alertVC animated:YES completion:nil];
+//                NSLog(@"登录成功");
+            }else{
+                //登录失败,弹出提示框
+                UIAlertController *alertVC = [UIAlertController alertControllerWithTitle:@"提示" message:@"用户名或密码错误" preferredStyle:UIAlertControllerStyleAlert];
+                UIAlertAction *yesAction = [UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:nil];
+                [alertVC addAction:yesAction];
+                [self presentViewController:alertVC animated:YES completion:nil];
+//                NSLog(@"登录失败");
+            }
+        }
+    }];
+}
+//点击屏幕
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
     [self.view endEditing:YES];
     
 }
+//跳转时触发
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender{
     RegisterViewController *registerVC = segue.destinationViewController;
     WK(weakSelf);
